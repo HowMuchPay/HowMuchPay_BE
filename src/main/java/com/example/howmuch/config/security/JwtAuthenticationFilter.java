6ca301e633eca943d8207377158d5f3c83781936
choidongkuen <danaver12@daum.net>
2023-07-10 04:02:30 +0900

    FEAT : JWT 관련 기본 구현(2차 구현 예정)

---
+++ src/main/java/com/example/howmuch/config/security/JwtAuthenticationFilter.java
@@ -0,0 +1,79 @@
+package com.example.howmuch.config.security;
+
+import io.jsonwebtoken.JwtException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+
+    private static final List<String> EXCLUDE_URL =
+            List.of("/members/login",
+                    "/members/join"
+            );
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws IOException, ServletException {
+
+        if (request.getRequestURI().contains("/h2-console")) {
+            chain.doFilter(request, response);
+        } else {
+
+
+            String token = resolveToken(request);
+
+        /*
+            jwtAuthenticationEntryPoint 로 넘어감.
+        */
+            if (token == null) {
+                log.error("JWT doesn't exist");
+                throw new JwtException("JWT is null!!");
+            }
+
+            if (this.jwtProvider.validateToken(token)) {
+                // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장
+                Authentication authentication = jwtProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                log.error("JWT is not valid");
+                throw new JwtException("JWT is not valid!");
+            }
+            chain.doFilter(request, response);
+        }
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws SecurityException {
+        return EXCLUDE_URL.stream().anyMatch(exclude -> exclude.equalsIgnoreCase(request.getRequestURI()));
+    }
+}
