60ae444926a8e96ebb95bd6ecee0ae4705c9a7a4
choidongkuen <danaver12@daum.net>
2023-07-10 04:01:57 +0900

    FEAT : 필요한 config 구현

---
+++ src/main/java/com/example/howmuch/config/web/WebConfig.java
@@ -0,0 +1,9 @@
+package com.example.howmuch.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class WebConfig {
+}
