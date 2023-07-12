261128e49499110fcc0ac0481aa0e2661de0e700
choidongkuen <danaver12@daum.net>
2023-07-13 00:12:26 +0900

    FEAT : feature3 merge 오류 해결

--- src/main/java/com/example/howmuch/config/app/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.howmuch.config.app;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
-@Configuration
-public class WebConfig {
-}
