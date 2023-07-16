b232c2f908896be6f75b95b13b2f76924c37619b
seongHyun-Min <steve6133@naver.com>
2023-07-16 18:41:57 +0900

    FEAT : CalculateAverageAmountRequestDto 추가

---
+++ src/main/java/com/example/howmuch/dto/recommednation/CalculateAverageAmountRequestDto.java
@@ -0,0 +1,25 @@
+package com.example.howmuch.dto.recommednation;
+
+import com.example.howmuch.contant.AcType;
+import com.example.howmuch.contant.EventCategory;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CalculateAverageAmountRequestDto {
+
+    @NotNull
+    private EventCategory eventCategory;
+
+    @NotNull
+    private AcType acquaintanceType;
+
+    @NotNull
+    private List<Boolean> intimacyAnswers;
+}
