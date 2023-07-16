7aed21310fac26f0f6f025f6f22550cda0d2dba5
seongHyun-Min <steve6133@naver.com>
2023-07-16 18:12:06 +0900

    REFACTOR : RecommendationEventService 반환 타입 변경 RecommendationEvent -> Long

--- src/main/java/com/example/howmuch/service/recommendation/RecommendationEventService.java
+++ src/main/java/com/example/howmuch/service/recommendation/RecommendationEventService.java
@@ -5,6 +5,7 @@ import com.example.howmuch.domain.repository.RecommendationEventRepository;
 import com.example.howmuch.dto.recommednation.RecommendationEventRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +20,7 @@ public class RecommendationEventService {
 
     //승현 : 추후 지인 경조사를 등록하는 과정에서 호출해도 좋을것 같다는 생각이 듭니다.
     @Transactional
-    public RecommendationEvent createRecommendationEvent(RecommendationEventRequestDto requestDto) {
+    public Long createRecommendationEvent(RecommendationEventRequestDto requestDto) {
         int intimacyLevel = calculateIntimacyLevel(requestDto.getIntimacyAnswers());
 
         RecommendationEvent recommendationEvent = RecommendationEvent.builder()
@@ -29,8 +30,10 @@ public class RecommendationEventService {
                 .intimacyLevel(intimacyLevel)
                 .build();
 
-        return recommendationEventRepository.save(recommendationEvent);
+        RecommendationEvent save = recommendationEventRepository.save(recommendationEvent);
+        return save.getId();
     }
+
 
     //YES, NO 질문을 통해 친밀도 계산 질문(5개)
     private int calculateIntimacyLevel(List<Boolean> intimacyAnswers) {
