7ca7e2b4a07566c2cbf55683bfd123ef2a535747
choidongkuen <danaver12@daum.net>
2023-07-24 01:58:26 +0900

    REFACTOR : 나의 경조사(MyEvent) 등록 API 리팩토링

--- src/main/java/com/example/howmuch/constant/MyType.java
+++ src/main/java/com/example/howmuch/constant/MyType.java
@@ -14,10 +14,9 @@ public enum MyType {
      * 0: 나의, 1: 부모의, 2:가족의, 3:자녀의
      */
 
-    MY(0, "나의"),
-    PARENT(1, "부모의"),
-    FAMILY(2, "가족의"),
-    CHILD(3, "자녀의");
+    MY(0, "나"),
+    PARENT(1, "가족"),
+    ETC(2, "기타");
 
     private final Integer value;
     private final String typeName;
--- src/main/java/com/example/howmuch/dto/event/CreateMyEventRequestDto.java
+++ src/main/java/com/example/howmuch/dto/event/CreateMyEventRequestDto.java
@@ -9,6 +9,7 @@ import lombok.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -18,12 +19,16 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class CreateMyEventRequestDto {
 
+    /**
+     * 1. EventCategory 에서 기타 선택 시 경조사명을 자유롭게 입력하는 화면 등장
+     */
+
     @NotNull
     private LocalDate eventAt;
 
     @NotNull
     @Min(0)
-    @Max(3)
+    @Max(2)
     private Integer myType;
 
     @NotNull
@@ -31,12 +36,21 @@ public class CreateMyEventRequestDto {
     @Max(4)
     private Integer eventCategory;
 
+    @Size(max = 255)
+    private String myEventName; // eventCategory 가 etc 인 경우
+
+    @Size(max = 255)
+    private String myEventCharacterName; // myType 이 '나' 가 아닌 경우
+
+
     public MyEvent toEntity(User user) {
         return MyEvent.builder()
                 .eventAt(eventAt)
                 .totalReceiveAmount(0L)
                 .myType(MyType.fromValue(myType))
                 .eventCategory(EventCategory.fromValue(eventCategory))
+                .myEventName(myEventName)
+                .myEventCharacterName(myEventCharacterName)
                 .user(user)
                 .build();
     }
