a9a6f70533e02ae30cc9dd488d72617a1b048306
seongHyun-Min <steve6133@naver.com>
2023-07-24 20:59:00 +0900

    refactor : findByOauthId -> findByPhoneNumber

--- src/main/java/com/example/howmuch/domain/repository/UserRepository.java
+++ src/main/java/com/example/howmuch/domain/repository/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByOauthId(String oauthId);
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
