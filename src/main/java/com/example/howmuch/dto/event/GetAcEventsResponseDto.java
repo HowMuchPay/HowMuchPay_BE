a7ff6356dae442861217ce9416b105820f2898d0
seongHyun-Min <steve6133@naver.com>
2023-08-08 15:21:42 +0900

    FEAT : 지인 경조사 단일 조회(디데이), 삭제 로직 추가

---
+++ src/main/java/com/example/howmuch/dto/event/GetAcEventsResponseDto.java
@@ -0,0 +1,35 @@
+package com.example.howmuch.dto.event;
+
+import com.example.howmuch.domain.entity.AcEvent;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAcEventsResponseDto {
+    private String nickname;
+
+    private LocalDate eventAt;
+
+    private int dDay;
+
+    private Long payAmount;
+
+    private Integer eventCategory;
+
+    private Integer AcType;
+
+    public static GetAcEventsResponseDto from(AcEvent acEvent, int dDay) {
+        return GetAcEventsResponseDto.builder()
+                .nickname(acEvent.getAcquaintanceNickname())
+                .eventAt(acEvent.getEventAt())
+                .dDay(dDay)
+                .payAmount(acEvent.getPayAmount())
+                .eventCategory(acEvent.getEventCategory().getValue())
+                .build();
+    }
+}
