357c000722e092246f702a92283fb094ac988db9
seongHyun-Min <steve6133@naver.com>
2023-08-29 16:33:48 +0900

    FEAT : calculatePercentage, findClosetEvent 로직 구현

--- src/main/java/com/example/howmuch/service/user/UserService.java
+++ src/main/java/com/example/howmuch/service/user/UserService.java
@@ -1,8 +1,11 @@
 package com.example.howmuch.service.user;
 
 
+import com.example.howmuch.domain.entity.AcEvent;
 import com.example.howmuch.domain.entity.User;
+import com.example.howmuch.domain.repository.AcEventRepository;
 import com.example.howmuch.domain.repository.UserRepository;
+import com.example.howmuch.dto.Home.HomeResponseDto;
 import com.example.howmuch.exception.user.NotFoundUserException;
 import com.example.howmuch.exception.user.NotMatchUserException;
 import com.example.howmuch.exception.user.PhoneNumberAlreadyExistsException;
@@ -13,6 +16,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -20,6 +26,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final RedisUtil redisUtil;
+    private final AcEventRepository acEventRepository;
 
 
     /**
@@ -54,6 +61,43 @@ public class UserService {
             throw new PhoneNumberAlreadyExistsException("이미 등록된 전화번호 입니다");
         }
         user.addPhoneNumber(phone);
+    }
+
+    @Transactional(readOnly = true)
+    public HomeResponseDto getHome() {
+        User user = findUserFromToken();
+
+        log.info(user.toString());
+        AcEvent closestEvent = findClosestEvent(user);
+        log.info(closestEvent.toString());
+
+        long pay = user.getUserTotalPayAmount();
+        long receive = user.getUserTotalReceiveAmount();
+
+        int payPercentage = calculatePayPercentage(pay, receive);
+
+        LocalDate currentDate = LocalDate.now();
+        int daysUntilEvent = (int) ChronoUnit.DAYS.between(currentDate, closestEvent.getEventAt());
+
+        return HomeResponseDto.builder()
+                .userTotalPayAmount(pay)
+                .userTotalReceiveAmount(receive)
+                .payPercentage(payPercentage)
+                .nickname(closestEvent.getAcquaintanceNickname())
+                .eventCategory(closestEvent.getEventCategory().getValue())
+                .dDay(daysUntilEvent)
+                .build();
+    }
+
+    private int calculatePayPercentage(long totalPayAmount, long totalReceiveAmount) {
+        if (totalPayAmount + totalReceiveAmount == 0) {
+            return 0; // 분모가 0인 경우 방지
+        }
+        return (int) (totalPayAmount / (totalPayAmount + totalReceiveAmount) * 100);
+    }
+
+    private AcEvent findClosestEvent(User user) {
+        return acEventRepository.findFirstByUserAndEventAtGreaterThanOrderByEventAtAsc(user, LocalDate.now()).orElse(null);
     }
 
     private boolean isPhoneNumberExists(String phone) {
