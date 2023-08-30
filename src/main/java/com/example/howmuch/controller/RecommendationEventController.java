9f5f61d80a52ee9cac3d8fa559d6fcd08277eb4d
seongHyun-Min <steve6133@naver.com>
2023-08-31 01:00:28 +0900

    REFACTOR : getRecommendation 컨트롤러 (요청 타입 변경 ReqeustBody -> RequestParam

--- src/main/java/com/example/howmuch/controller/RecommendationEventController.java
+++ src/main/java/com/example/howmuch/controller/RecommendationEventController.java
@@ -2,7 +2,6 @@ package com.example.howmuch.controller;
 
 
 import com.example.howmuch.dto.recommednation.CreateAverageAmountRequestDto;
-import com.example.howmuch.dto.recommednation.GetAppAverageAmountRequestDto;
 import com.example.howmuch.dto.recommednation.GetAverageAmountRequestDto;
 import com.example.howmuch.service.recommendation.RecommendationEventService;
 import lombok.RequiredArgsConstructor;
@@ -10,12 +9,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 
 @Controller
@@ -29,24 +28,48 @@ public class RecommendationEventController {
 
     @GetMapping("/get")
     public ResponseEntity<Integer> getRecommendation(
-            @RequestBody @Valid GetAverageAmountRequestDto requestDto) {
+            @RequestParam @NotNull Integer ageGroup,
+            @RequestParam @NotNull Integer annualIncome,
+            @RequestParam @NotNull @Min(0) @Max(4) Integer eventCategory,
+            @RequestParam @NotNull @Min(0) @Max(4) Integer acquaintanceType,
+            @RequestParam @NotNull @Min(1) @Max(5) Integer intimacyLevel) {
+
+        GetAverageAmountRequestDto requestDto = GetAverageAmountRequestDto.builder()
+                .ageGroup(ageGroup)
+                .annualIncome(annualIncome)
+                .eventCategory(eventCategory)
+                .acquaintanceType(acquaintanceType)
+                .intimacyLevel(intimacyLevel)
+                .build();
+
         double recommendationAmount = recommendationEventService.getRecommendationEvent(requestDto);
         return ResponseEntity.ok((int) recommendationAmount);
     }
 
-
     @PostMapping("/save")
-    public ResponseEntity<Void> SaveRecommendation(
+    public ResponseEntity<Void> saveRecommendation(
             @RequestBody @Valid CreateAverageAmountRequestDto requestDto) {
         recommendationEventService.createRecommendationEvent(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-
     @GetMapping("/app-get")
-    public ResponseEntity<Integer> getRecommendationApp(
-            @RequestBody @Valid GetAppAverageAmountRequestDto requestDto) {
-        double recommendationAmount = recommendationEventService.getRecommendationApp(requestDto);
+    public ResponseEntity<Integer> getAppRecommendation(
+            @RequestParam @NotNull Integer ageGroup,
+            @RequestParam @NotNull Integer annualIncome,
+            @RequestParam @NotNull @Min(0) @Max(4) Integer eventCategory,
+            @RequestParam @NotNull @Min(0) @Max(4) Integer acquaintanceType,
+            @RequestParam @NotNull @Min(1) @Max(5) Integer intimacyLevel) {
+
+        GetAverageAmountRequestDto requestDto = GetAverageAmountRequestDto.builder()
+                .ageGroup(ageGroup)
+                .annualIncome(annualIncome)
+                .eventCategory(eventCategory)
+                .acquaintanceType(acquaintanceType)
+                .intimacyLevel(intimacyLevel)
+                .build();
+
+        double recommendationAmount = recommendationEventService.getRecommendationEvent(requestDto);
         return ResponseEntity.ok((int) recommendationAmount);
     }
 }
