424b4b9380dd6901fd42bac77ff4bdde0cf49d63
seongHyun-Min <steve6133@naver.com>
2023-08-30 16:56:28 +0900

    FEAT : addCorsMappings 추가 React 통신

--- src/main/java/com/example/howmuch/config/security/CorsConfig.java
+++ src/main/java/com/example/howmuch/config/security/CorsConfig.java
@@ -5,9 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
 
     @Bean
     public CorsFilter corsFilter() {
