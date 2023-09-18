bd67accd2c0922687fb9f2f8b9c12c77159d9d77
seongHyun-Min <steve6133@naver.com>
2023-09-18 16:16:40 +0900

    feat : 나의 경조사 세부 사항 조회 (페치조인)

--- src/main/java/com/example/howmuch/domain/repository/MyEventDetailRepository.java
+++ src/main/java/com/example/howmuch/domain/repository/MyEventDetailRepository.java
@@ -2,12 +2,18 @@ package com.example.howmuch.domain.repository;
 
 import com.example.howmuch.domain.entity.MyEvent;
 import com.example.howmuch.domain.entity.MyEventDetail;
+import com.example.howmuch.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MyEventDetailRepository extends JpaRepository<MyEventDetail, Long> {
 
+    // 나의 경조사 세부 사항 조회 (페치조인)
+    @Query("select md from MyEventDetail md join fetch md.myEvent where md.user = :user")
+    List<MyEventDetail> findAllByUserOrderByCreatedAtDesc(@Param("user") User user);
     // 나의 경조사 세부 사항 오름차순
     List<MyEventDetail> findAllByMyEventOrderByReceiveAmountAsc(MyEvent myEvent);
 
