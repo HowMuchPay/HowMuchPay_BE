35364330efc387a54ee3a9739ee9b2d70f8dadcb
seongHyun-Min <steve6133@naver.com>
2023-10-01 00:15:01 +0900

    feat : CombinedEventResponseDto 추가

---
+++ src/main/java/com/example/howmuch/dto/home/CombinedEventResponse.java
@@ -0,0 +1,30 @@
+package com.example.howmuch.dto.home;
+
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+public class CombinedEventResponse {
+
+    private Long id;
+    private String displayName;
+    private LocalDate eventAt;
+    private Long amount;
+    private int category;
+    private String eventType;
+
+    public CombinedEventResponse(Long id, String displayName, LocalDate eventAt, Long amount, int category, String eventType) {
+        this.id = id;
+        this.displayName = displayName;
+        this.eventAt = eventAt;
+        this.amount = amount;
+        this.category = category;
+        this.eventType = eventType;
+    }
+}
