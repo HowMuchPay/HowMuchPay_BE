31e140b65e16976a3a8e68a16a9fac5c50dc5679
seongHyun-Min <steve6133@naver.com>
2023-10-05 16:16:33 +0900

    refactor : getPayAmountByRecommendationEvent -> getMostSelectedPayAmountByRecommendationEvent

--- src/main/java/com/example/howmuch/domain/repository/RecommendationEventRepository.java
+++ src/main/java/com/example/howmuch/domain/repository/RecommendationEventRepository.java
@@ -3,6 +3,7 @@ package com.example.howmuch.domain.repository;
 import com.example.howmuch.constant.AcType;
 import com.example.howmuch.constant.EventCategory;
 import com.example.howmuch.domain.entity.RecommendationEvent;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,4 +16,12 @@ public interface RecommendationEventRepository extends JpaRepository<Recommendat
     Optional<Double> getPayAmountByRecommendationEvent(EventCategory eventCategory, AcType acquaintanceType,
                                                int minIntimacyLevel, int maxIntimacyLevel,
                                                int AgeGroup, int AnnualIncome);
+
+    @Query("SELECT r.payAmount, COUNT(*) FROM RecommendationEvent r WHERE r.eventCategory = ?1 AND r.acquaintanceType = ?2 AND " +
+        "r.intimacyLevel BETWEEN ?3 AND ?4 AND r.ageGroup = ?5 AND r.annualIncome = ?6 " +
+        "GROUP BY r.payAmount " +
+        "ORDER BY COUNT(*) DESC")
+    List<Object[]> getMostSelectedPayAmountByRecommendationEvent(EventCategory eventCategory, AcType acquaintanceType,
+        int minIntimacyLevel, int maxIntimacyLevel,
+        int AgeGroup, int AnnualIncome);
 }
